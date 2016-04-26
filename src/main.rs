158f7f03c076d236d0968cf38c7b51c0025b4d51
skyshk <skyshk@gmail.com>
2016-04-27 03:19:16 +0900

    create Frog class

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+// Proverb-Programming: "Frog bears Frogs"
+
+struct Frog {
+    is_frog: bool
+}
+
+impl Frog {
+    // constructor
+    pub fn new() -> Frog {
+        Frog{
+            is_frog: true
+        }
+    }
+    pub fn is_frog(&self)->bool{
+        self.is_frog
+    }
+
+    pub fn bears_child_frog (&self) -> Frog{
+        Frog{
+            is_frog: true
+        }        
+    }
+}
+
+fn main() {
+    let n_max_frogs = 4096;
+    let mut frogs: Vec<Frog> = Vec::new();
+    let first_frog = Frog::new();
+    frogs.push(first_frog);
+
+    let mut cnt = 0;
+    loop{
+        let mut new_frogs : Vec<Frog> = Vec::new();
+        for frog in &frogs {
+            let child_frog = frog.bears_child_frog();
+            new_frogs.push(child_frog);
+        }
+        frogs.append(&mut new_frogs);
+        cnt += 1;
+        println!("Current number of frogs is: {}", frogs.len());
+        if frogs.len() >= n_max_frogs{break;}
+    }
+    println!("The pond get fulfilled.");
+}
